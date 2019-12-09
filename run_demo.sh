997f2455a19803a5975777dc759f0472b18a2625
zingp <605156398@qq.com>
2019-12-09 22:30:51 +0800

    fmt

--- run_demo.sh
+++ run_demo.sh
@@ -2,7 +2,7 @@ python main.py --status train --train ./data/demo.train.char --dev ./data/demo.d
 
 
 # python main.py --status decode \
-# 		--raw ../data/onto4ner.cn/demo.test.char \
-# 		--savedset ../data/onto4ner.cn/demo.dset \
-# 		--loadmodel ../data/onto4ner.cn/demo.0.model \
-# 		--output ../data/onto4ner.cn/demo.raw.out \
+# --raw ../data/onto4ner.cn/demo.test.char \
+# --savedset ../data/onto4ner.cn/demo.dset \
+# --loadmodel ../data/onto4ner.cn/demo.0.model \
+# --output ../data/onto4ner.cn/demo.raw.out \
